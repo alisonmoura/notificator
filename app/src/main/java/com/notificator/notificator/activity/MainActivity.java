fd279a1ffc0614a2737609ef4cd25854f792c341
Marcel Paulino <mlpaulino@gmail.com>
2017-11-11 11:55:08 -0300

    Drawer Incompleto

--- app/src/main/java/com/notificator/notificator/activity/MainActivity.java
+++ app/src/main/java/com/notificator/notificator/activity/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.notificator.notificator.R;
@@ -15,11 +18,26 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity{
 
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
         ButterKnife.bind(this);
+
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mPlanetTitles));
+        // Set the list's click listener
+        // mDrawerList.setOnItemClickListener(new DrawerLayout.);
+
     }
 
     @OnClick(R.id.btn_notificacoes)
