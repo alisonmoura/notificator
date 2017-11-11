1b4415b23542ec1299ab21842a4362c7813af603
Alison Moura <alison.oghino@gmail.com>
2017-11-11 09:39:04 -0300

    Adicionando o ButterKnife

--- app/src/main/java/com/notificator/notificator/activity/MainActivity.java
+++ app/src/main/java/com/notificator/notificator/activity/MainActivity.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.notificator.notificator.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -13,5 +18,15 @@ public class MainActivity extends AppCompatActivity{
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.btn_notificacoes)
+    public void irParaNotificacao(){
+        System.out.println("Clicou no botão");
+        TextView textoInicial = (TextView)findViewById(R.id.text_view_mensagem_inicial);
+        textoInicial.setText("Clicou no botão de notificações");
+    }
+
+
 }
