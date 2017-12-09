30a11298ee82dc0892af7b20054f2d8839a86e7b
Danielle Ferreira <danianaliseuniderp@gmail.com>
2017-12-09 09:33:30 -0300

    Criando Classe Usuario

---
+++ app/src/main/java/com/notificator/notificator/model/Usuario.java
@@ -0,0 +1,74 @@
+package com.notificator.notificator.model;
+
+
+public class Usuario {
+
+    private Integer id;
+    private String nome;
+    private String email;
+    private String foto;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Usuario usuario = (Usuario) o;
+
+        if (id != null ? !id.equals(usuario.id) : usuario.id != null) return false;
+        if (nome != null ? !nome.equals(usuario.nome) : usuario.nome != null) return false;
+        if (email != null ? !email.equals(usuario.email) : usuario.email != null) return false;
+        return foto != null ? foto.equals(usuario.foto) : usuario.foto == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (nome != null ? nome.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (foto != null ? foto.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", foto='" + foto + '\'' +
+                '}';
+    }
+}
