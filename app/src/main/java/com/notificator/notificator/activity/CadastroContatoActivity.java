a5a04088dfa3c885a9fe63f3815386c3573214c5
Alison Moura <alison.oghino@gmail.com>
2017-12-09 11:47:47 -0300

    Colocando datepicker no cadastro de contato

--- app/src/main/java/com/notificator/notificator/activity/CadastroContatoActivity.java
+++ app/src/main/java/com/notificator/notificator/activity/CadastroContatoActivity.java
@@ -1,17 +1,24 @@
 package com.notificator.notificator.activity;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.notificator.notificator.R;
 import com.notificator.notificator.model.Contato;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -37,6 +44,9 @@ public class CadastroContatoActivity extends AppCompatActivity {
     EditText msgniver;
     @Bind(R.id.cadastro_contato_notificar)
     CheckBox notificar;
+
+    Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener date;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState  ) {
@@ -66,6 +76,35 @@ public class CadastroContatoActivity extends AppCompatActivity {
             notificar.setChecked(contato.getNotificarAniversario());
         }
 
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        aniversario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(CadastroContatoActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+    }
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        aniversario.setText(sdf.format(myCalendar.getTime()));
     }
 
     @OnClick(R.id.btn_cancelar)
