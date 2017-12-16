b1651fdc73849ac2a4e292b5b917e83d94040b01
Alison Moura <alison.oghino@gmail.com>
2017-12-16 11:49:17 -0300

    Adicionando mapa no cadastro de notificação

--- app/src/main/java/com/notificator/notificator/activity/CadastroNotificacaoActivity.java
+++ app/src/main/java/com/notificator/notificator/activity/CadastroNotificacaoActivity.java
@@ -1,5 +1,7 @@
 package com.notificator.notificator.activity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
@@ -7,13 +9,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.notificator.notificator.R;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class CadastroNotificacaoActivity extends AppCompatActivity {
+
+    private GoogleMap mMap;
+    private LatLng latLng;
+    private Marker marker;
+    private String endereco;
+    Geocoder geocoder;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,12 +50,67 @@ public class CadastroNotificacaoActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        setUpMapIfNeeded();
     }
 
-    @OnClick(R.id.btn_salvar)
-    public void irParaNotificar(){
-        TextView textoNotificar = (TextView)findViewById(R.id.txt_de_envio);
-        textoNotificar.setText("Clicou no botão Salvar");
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng point) {
+                //save current location
+                latLng = point;
+
+                List<Address> addresses = new ArrayList<>();
+                try {
+                    addresses = geocoder.getFromLocation(point.latitude, point.longitude,1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                android.location.Address address = addresses.get(0);
+
+                if (address != null) {
+                    StringBuilder enderecoEscolhido = new StringBuilder();
+                    for (int i = 0; i < address.getMaxAddressLineIndex(); i++){
+                        enderecoEscolhido.append(address.getAddressLine(i) + "\n");
+                    }
+                    Toast.makeText(CadastroNotificacaoActivity.this, enderecoEscolhido.toString(), Toast.LENGTH_LONG).show();
+                    endereco = enderecoEscolhido.toString();
+                }
+
+                if (marker != null) {
+                    marker.remove();
+                }
+
+                marker = mMap.addMarker(new MarkerOptions().position(point).title("Marker")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+            }
+        });
+
+    }
+
+    @OnClick(R.id.cadastro_notificacao_btn_salvar)
+    public void salvar(){
+        System.out.println(endereco);
     }
 
     @OnClick(R.id.btnCancelar)
